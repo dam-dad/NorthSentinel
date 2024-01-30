62841dc7906199ea888f57e330cd2647be1f557f
samupad <83948765+samupad@users.noreply.github.com>
2024-01-30 12:58:14 +0000

    V1.1
    Actualización del archivo POM, creación inicial del Main y el APP.

--- src/main/java/dad/northsentinel/main/App.java
+++ src/main/java/dad/northsentinel/main/App.java
@@ -1,5 +1,19 @@
 package dad.northsentinel.main;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	
+	public void start(Stage primaryStage) throws Exception {
+		
+			
+		}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
+//Autores: Iván Garrido, Feliciano Pérez y Samuel Padrón.
+
--- src/main/java/dad/northsentinel/main/Main.java
+++ src/main/java/dad/northsentinel/main/Main.java
@@ -1,5 +1,10 @@
 package dad.northsentinel.main;
-
+//Clase Main para la ejecución final del juego
 public class Main {
+	public static void main(String[] args) {
+		App.main(args);
+
+	}
 
 }
+//Autores: Iván Garrido, Feliciano Pérez y Samuel Padrón

091afaf535ef75ce39a95510226cfaa530e7d08b
samupad <83948765+samupad@users.noreply.github.com>
2024-02-20 12:30:41 +0000

    Solución error Volumen
    Se ha solventado un error que no permitía modificar el volumen desde los ajustes del juego.

--- src/main/java/dad/northsentinel/mainmenu/SettingsController.java
+++ src/main/java/dad/northsentinel/mainmenu/SettingsController.java
@@ -19,66 +19,74 @@ import javafx.fxml.Initializable;
 
 public class SettingsController implements Initializable {
 
-	private EventHandler<ActionEvent> onGoBack;
-	
-	private MainController mainController;
+    private EventHandler<ActionEvent> onGoBack;
+    
+    private MainController mainController;
 
-	@FXML
-	private BorderPane view;
+    @FXML
+    private BorderPane view;
 
-	@FXML
-	private ImageView volumeImage;
+    @FXML
+    private ImageView volumeImage;
 
-	@FXML
-	private Label volumeLabel;
+    @FXML
+    private Label volumeLabel;
 
-	@FXML
-	private Slider volumeSlider;
-	
-	public SettingsController() {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/SettingsView.fxml"));
-		loader.setController(this);
-		try {
-			loader.load();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    @FXML
+    private Slider volumeSlider;
+    
+    public SettingsController() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/SettingsView.fxml"));
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@FXML
-	void onGoBack(ActionEvent event) {
-		if (onGoBack != null) {
-			onGoBack.handle(event);
-		}
-	}
+    @FXML
+    void onGoBack(ActionEvent event) {
+        if (onGoBack != null) {
+            onGoBack.handle(event);
+        }
+    }
 
-	public void setOnGoBack(EventHandler<ActionEvent> onGoBack) {
-		this.onGoBack = onGoBack;
-	}
+    public void setOnGoBack(EventHandler<ActionEvent> onGoBack) {
+        this.onGoBack = onGoBack;
+    }
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-	    volumeLabel.textProperty().bind(Bindings.format("%.0f", volumeSlider.valueProperty()));
-	    
-	    volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
-	        double volume = newValue.doubleValue() / 100;
-	        System.out.println(volume);
-	        mainController.getMediaPlayerMenu().setVolume(volume);
-	        
-	        if (newValue.doubleValue() > 75) {
-	            volumeImage.setImage(new Image(getClass().getResource("/images/musica.png").toExternalForm()));
-	        } else if (newValue.doubleValue() > 25) {
-	            volumeImage.setImage(new Image(getClass().getResource("/images/altavoz2.png").toExternalForm()));
-	        } else if (newValue.doubleValue() > 1) {
-	            volumeImage.setImage(new Image(getClass().getResource("/images/altavoz1.png").toExternalForm()));
-	        } else if (newValue.doubleValue() == 0) {
-	            volumeImage.setImage(new Image(getClass().getResource("/images/nomusica.png").toExternalForm()));
-	        }
-	    });
-	}
+    // Método setter para mainController
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
 
-	public BorderPane getView() {
-		return view;
-	}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        volumeLabel.textProperty().bind(Bindings.format("%.0f", volumeSlider.valueProperty()));
+        
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (mainController != null) { // Verificar que mainController no sea null
+                double volume = newValue.doubleValue() / 100;
+                //System.out.println(volume); Depuración por consola.
+                mainController.getMediaPlayerMenu().setVolume(volume);
+                
+                if (newValue.doubleValue() > 75) {
+                    volumeImage.setImage(new Image(getClass().getResource("/images/musica.png").toExternalForm()));
+                } else if (newValue.doubleValue() > 25) {
+                    volumeImage.setImage(new Image(getClass().getResource("/images/altavoz2.png").toExternalForm()));
+                } else if (newValue.doubleValue() > 1) {
+                    volumeImage.setImage(new Image(getClass().getResource("/images/altavoz1.png").toExternalForm()));
+                } else if (newValue.doubleValue() == 0) {
+                    volumeImage.setImage(new Image(getClass().getResource("/images/nomusica.png").toExternalForm()));
+                }
+            }
+        });
+    }
+
+    public BorderPane getView() {
+        return view;
+    }
 
 }
+
